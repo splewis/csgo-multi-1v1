4b486554a900233ca028bc192014da427bf11178
Clayton <supimfuzzy@users.noreply.github.com>
2014-06-09 19:23:55 -0400

    Update and rename connect.inc.php to config.inc.php

--- web/includes/config.inc.php
+++ web/includes/config.inc.php
@@ -5,7 +5,7 @@ $mysql_pass = '';
 $mysql_db = 'multi1v1';
 
 $page_title = "Multi 1v1 Arena";
-$SteamAPI_Key = "56D18EBECA7C9BE07801F285CE76DB2D"; //https://steamcommunity.com/dev/apikey
+$SteamAPI_Key = ""; //https://steamcommunity.com/dev/apikey
 
 if (@!mysql_connect($mysql_host,$mysql_user,$mysql_pass) || @!mysql_select_db($mysql_db)){
 	die('Cannot connect.');
