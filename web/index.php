55caf55bbab2860a43532d263e8eeb386f771cf4
Clayton <supimfuzzy@users.noreply.github.com>
2014-06-13 17:33:22 -0400

    Added rank, player activity, moved functions

--- web/index.php
+++ web/index.php
@@ -4,7 +4,8 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 
 <?php 
-	require 'includes/config.inc.php';
+	require_once 'includes/config.inc.php';
+	require_once 'includes/utils.inc.php';
 	echo "<title>$page_title</title>";
 	$time = microtime();
 	$time = explode(' ', $time);
@@ -20,7 +21,7 @@
 	<div class="everything">
 
 		<?php
-		if(!isset($_GET['id']) && empty($_GET['id'])){
+		if(!isset($_GET['id']) && empty($_GET['id']) || $_GET['id']==''){
 			echo "<h3>$page_title</h3>";
 			echo "<div class=\"searchnoinfo\">";
 		}else{
@@ -35,86 +36,45 @@
 		</div>
 
 		<?php
-		if(!isset($_GET['id']) && empty($_GET['id'])){
-			echo "<div class=\"topPlayers\">";
+		if(!isset($_GET['id']) && empty($_GET['id']) || $_GET['id']==''){
+			echo "<div class=\"topPlayers\"><h1>Top 15 Players</h1>";
 			include 'includes/generatetopplayers.php';
 			echo "</div>";
 		}
 
 		if(isset($_GET['id']) && !empty($_GET['id'])){
+			$totalplayers = mysql_num_rows(mysql_query("SELECT * FROM $mysql_table"));
 			$searchquery = htmlentities($_GET['id']);
-			@$query = "SELECT * FROM multi1v1_stats WHERE accountID='$searchquery'";
+			$query = "SELECT s1.*, (SELECT COUNT(*) FROM $mysql_table AS s2 WHERE s2.rating > s1.rating)+1 AS rank FROM $mysql_table AS s1 WHERE accountID=".mysql_real_escape_string($searchquery);
 			if(mysql_fetch_row(mysql_query($query))>0){
-				if(@$query_run = mysql_query($query)){
+				if($query_run = mysql_query($query)){
 					while ($row = mysql_fetch_assoc($query_run)){
 						$accountID = $row['accountID'];
 						$auth = $row['auth'];
+						$rank = $row['rank'];
 						$name = $row['name'];
 						$wins = $row['wins'];
 						$losses = $row['losses'];
 						$rating = $row['rating'];
+						$lastTime = $row['lastTime'];
 
 						echo "<h3>Stats for player: <a href=\"http://steamcommunity.com/profiles/".GetCommunityID($auth)."\">$name</a></h3>
 						<div class=\"stats\">
+						<h4>Rank: $rank of $totalplayers</h4><br>
 						<h4>Wins: $wins</h4><br>
 						<h4>Losses: $losses</h4><br>
 						<h4>W/L Ratio: ".number_format((float)$wins/$losses, 2, '.', '')."</h4><br>
 						<h4>ELO Rating: $rating</h4><br>
-						<div class=\"playerAvatar ".getPlayerState(GetCommunityID($auth))."\"><a href=\"http://steamcommunity.com/profiles/".GetCommunityID($auth)."\"><img src=\"".getAvatar(GetCommunityID($auth))."\"/></a></div>";
+						<div class=\"playerAvatar ".getPlayerState(GetCommunityID($auth))."\"><a href=\"http://steamcommunity.com/profiles/".GetCommunityID($auth)."\"><img src=\"".getAvatar(GetCommunityID($auth))."\"/></a>";
+						if(isPlayerActive($lastTime) == true){
+							echo "<div class=\"activity inactive\">INACTIVE</div></div>";
+						}else{
+							echo "<div class=\"activity\">ACTIVE</div></div>";
+						}
 					}
 				}
 			}else{
-				die("User not found.");
-			}
-		}
-
-		function GetCommunityID($steamid){
-			$steamidexplode = explode(':', str_replace('STEAM_', '' ,$steamid)); 
-			$commid = substr(bcadd(bcadd('76561197960265728', $steamidexplode['1']), bcmul($steamidexplode['2'], '2')),0, 17);
-			return $commid;
-		}
-
-		function getAvatar($commid){
-			global $SteamAPI_Key;
-			$url = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key='.$SteamAPI_Key.'&steamids='.$commid;
-
-			$cURL = curl_init();
-			curl_setopt($cURL, CURLOPT_URL, $url);
-			curl_setopt($cURL, CURLOPT_HTTPGET, true);
-			curl_setopt($cURL, CURLOPT_RETURNTRANSFER, TRUE);
-			curl_setopt($cURL, CURLOPT_HTTPHEADER, array(
-			    'Content-Type: application/json',
-			    'Accept: application/json'
-			));
-			$result = curl_exec($cURL);
-			$json = json_decode($result, true);
-
-			foreach($json['response']['players'] as $item) {
-				return $item['avatarfull'];
-			}
-		}
-
-		function getPlayerState($commid){
-			global $SteamAPI_Key;
-			$url = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key='.$SteamAPI_Key.'&steamids='.$commid;
-
-			$cURL = curl_init();
-			curl_setopt($cURL, CURLOPT_URL, $url);
-			curl_setopt($cURL, CURLOPT_HTTPGET, true);
-			curl_setopt($cURL, CURLOPT_RETURNTRANSFER, TRUE);
-			curl_setopt($cURL, CURLOPT_HTTPHEADER, array(
-			    'Content-Type: application/json',
-			    'Accept: application/json'
-			));
-			$result = curl_exec($cURL);
-			$json = json_decode($result, true);
-
-			foreach($json['response']['players'] as $item) {
-				if($item['personastate'] == 0){
-					return "offline";
-				}else{
-					return "online";
-				}
+				die("<h1>User not found.</h1>");
 			}
 		}
 		?>
