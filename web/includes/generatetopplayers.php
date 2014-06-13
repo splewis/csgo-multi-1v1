71b748bcc6d48676e1b58261b0ba0bad5a16f39a
Clayton <supimfuzzy@users.noreply.github.com>
2014-06-13 17:36:13 -0400

    Display if player is active/inactive in top 15.

--- web/includes/generatetopplayers.php
+++ web/includes/generatetopplayers.php
@@ -1,10 +1,10 @@
 <?php
 require 'includes/config.inc.php';
-//tg-bsv2 Light
-//tg-dilm Dark
+
 $generated = "<table class=\"tg\" width=\"450px\">
 	<tr>
-		<th class=\"tg-dilm\" width=\"150\">Name</th>
+		<th class=\"tg-dilm\">#</th>
+		<th class=\"tg-dilm\" width=\"200\">Name</th>
 		<th class=\"tg-dilm\">Wins</th>
 		<th class=\"tg-dilm\">Losses</th>
 		<th class=\"tg-dilm\">W/L Ratio</th>
@@ -13,39 +13,50 @@ $generated = "<table class=\"tg\" width=\"450px\">
 
 $alt = 0;
 
-$query = "SELECT * FROM multi1v1_stats ORDER BY rating DESC";
+$query = "SELECT s1.*, (SELECT COUNT(*) FROM multi1v1_stats AS s2 WHERE s2.rating > s1.rating)+1 AS rank FROM multi1v1_stats AS s1 ORDER BY rating DESC";
 
 if(@$query_run = mysql_query($query)){
 	for ($i=0; $i < 15; $i++) {
 		$row = mysql_fetch_assoc($query_run);
 		$accountID = $row['accountID'];
-		$auth = $row['auth'];
+		$rank = $row['rank'];
 		$name = $row['name'];
 		$wins = $row['wins'];
 		$losses = $row['losses'];
 		$rating = $row['rating'];
+		$lastTime = $row['lastTime'];
 		
 		if($alt == 0){
-			$generated .= "<tr>
-							<td class=\"tg-bsv2\"><a href=\"index.php?id=".$accountID."\">$name</a></td>
-							<td class=\"tg-bsv2\">$wins</td>
+			$generated .= "<tr><td class=\"tg-bsv2\">$rank</td>";
+
+			if(isPlayerActive($lastTime) == true){
+				$generated .= "<td class=\"tg-bsv2\"><a href=\"index.php?id=".$accountID."\">$name (Inactive)</a></td>";
+			}else{
+				$generated .= "<td class=\"tg-bsv2\"><a href=\"index.php?id=".$accountID."\">$name</a></td>";
+			}
+
+			$generated .= "<td class=\"tg-bsv2\">$wins</td>
 							<td class=\"tg-bsv2\">$losses</td>
-							<td class=\"tg-bsv2\">$wins/$losses</td>
-							<td class=\"tg-bsv2\">$rating</td>
-							</tr>";
+							<td class=\"tg-bsv2\">".number_format((float)$wins/$losses, 2, '.', '')."</td>
+							<td class=\"tg-bsv2\">$rating</td></tr>";
 			$alt = 1;
 		}else{
-			$generated .= "<tr>
-							<td class=\"tg-dilm\"><a href=\"index.php?id=".$accountID."\">$name</a></td>
-							<td class=\"tg-dilm\">$wins</td>
+			$generated .= "<tr><td class=\"tg-dilm\">$rank</td>";
+
+			if(isPlayerActive($lastTime) == true){
+				$generated .= "<td class=\"tg-dilm\"><a href=\"index.php?id=".$accountID."\">$name (Inactive)</a></td>";
+			}else{
+				$generated .= "<td class=\"tg-dilm\"><a href=\"index.php?id=".$accountID."\">$name</a></td>";
+			}
+
+			$generated .= "<td class=\"tg-dilm\">$wins</td>
 							<td class=\"tg-dilm\">$losses</td>
-							<td class=\"tg-dilm\">$wins/$losses</td>
-							<td class=\"tg-dilm\">$rating</td>
-							</tr>";
+							<td class=\"tg-dilm\">".number_format((float)$wins/$losses, 2, '.', '')."</td>
+							<td class=\"tg-dilm\">$rating</td></tr>";
 			$alt = 0;
 		}
 	}
-	$generated .= "</table>";
+	$generated .= "</table><br>Ranked by ELO Rating.";
 }
 echo $generated;
 ?>
