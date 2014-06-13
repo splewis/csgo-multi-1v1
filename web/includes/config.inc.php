33aa0fca30009fb2326013d59e452c7bf93b1945
Clayton <supimfuzzy@users.noreply.github.com>
2014-06-13 17:32:05 -0400

    Added new settings

--- web/includes/config.inc.php
+++ web/includes/config.inc.php
@@ -2,7 +2,7 @@
 /**
  * =============================================================================
  * @author Clayton
- * @version 1.2.0
+ * @version 1.3.0
  * @link https://github.com/supimfuzzy/csgo-multi-1v1
  * =============================================================================
  */
@@ -11,6 +11,7 @@ $mysql_host = 'localhost:3306';
 $mysql_user = 'root';
 $mysql_pass = '';
 $mysql_db = '';
+$mysql_table = 'multi1v1_stats'; //Default table set by game plugin. Do not change this if you don't know what you are doing.
 
 $page_title = "Multi 1v1 Arena";
 $SteamAPI_Key = ""; //https://steamcommunity.com/dev/apikey
@@ -18,7 +19,6 @@ $elo_rate_loss = 40; //Rate loss constant. A higher value equals a higher rate l
 $log_antisquatter = true; //Keep IP logs and the number of player stats changes. For debugging and testing purposes only.
 $days_until_inactivity = 7; //Number of days before a player is given an inactive status.
 
-//The @ symbol removed error reporting from that line.
 if (!mysql_connect($mysql_host,$mysql_user,$mysql_pass) || !mysql_select_db($mysql_db)){
 	die('Cannot connect.');
 }
