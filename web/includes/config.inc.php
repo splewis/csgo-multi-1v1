e0a81b4f9f5aa041f895cbf9bb8de5a36c64e4f4
Clayton <supimfuzzy@users.noreply.github.com>
2014-06-13 17:21:56 -0400

    Added new settings

--- web/includes/config.inc.php
+++ web/includes/config.inc.php
@@ -7,15 +7,19 @@
  * =============================================================================
  */
 
-$mysql_host = 'localhost';
+$mysql_host = 'localhost:3306';
 $mysql_user = 'root';
 $mysql_pass = '';
 $mysql_db = '';
 
 $page_title = "Multi 1v1 Arena";
 $SteamAPI_Key = ""; //https://steamcommunity.com/dev/apikey
+$elo_rate_loss = 40; //Rate loss constant. A higher value equals a higher rate loss per day of inactivity.
+$log_antisquatter = true; //Keep IP logs and the number of player stats changes. For debugging and testing purposes only.
+$days_until_inactivity = 7; //Number of days before a player is given an inactive status.
 
-if (@!mysql_connect($mysql_host,$mysql_user,$mysql_pass) || @!mysql_select_db($mysql_db)){
+//The @ symbol removed error reporting from that line.
+if (!mysql_connect($mysql_host,$mysql_user,$mysql_pass) || !mysql_select_db($mysql_db)){
 	die('Cannot connect.');
 }
 ?>
