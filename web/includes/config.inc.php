0d50195a055914bd68b2b0ffa829ec78926d9433
Clayton <supimfuzzy@users.noreply.github.com>
2014-06-13 18:02:47 -0400

    Update config.inc.php

--- web/includes/config.inc.php
+++ web/includes/config.inc.php
@@ -15,7 +15,7 @@ $mysql_table = 'multi1v1_stats'; //Default table set by game plugin. Do not chan
 
 $page_title = "Multi 1v1 Arena";
 $SteamAPI_Key = ""; //https://steamcommunity.com/dev/apikey
-$elo_rate_loss = 40; //Rate loss constant. A higher value equals a higher rate loss per day of inactivity.
+$antisquatter_rate_loss = 40; //Rate loss constant. A higher value equals a higher rate loss per day of inactivity.
 $log_antisquatter = true; //Keep IP logs and the number of player stats changes. For debugging and testing purposes only.
 $days_until_inactivity = 7; //Number of days before a player is given an inactive status.
 
