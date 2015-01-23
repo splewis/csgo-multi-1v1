3fb388d9e350081be5c325489c935bbde8d8d4af
Sean Lewis <splewis@utexas.edu>
2015-01-23 17:31:11 -0600

    Tweak web redirect page to always popup.

--- web/redirect.php
+++ web/redirect.php
@@ -2,18 +2,14 @@
 <head>
 <?php
 
-if (isset($_GET['id']) && !empty($_GET['id'])) {
-	$id = $_GET['id'];
-	$u = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-	$url = str_replace("redirect.php", "index.php", $u);
+$url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+$url = str_replace("redirect.php", "index.php", $url);
 
 echo "<script type=\"text/javascript\">
 window.onload = function(){
-    var popup=window.open(\"$url\",\"Test Page :D\");
+	   var popup=window.open(\"$url\",\"Player Stats\");
 };
 </script>";
-
-}
 
 ?>
 </head>
