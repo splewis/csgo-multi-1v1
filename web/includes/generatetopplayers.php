9508fa3f545ecc1abcfa698a099267d431d85ccb
Ryan <ryan@ryanpope.com>
2015-12-14 14:38:03 -0600

    Adding bootstrap to project
    Allows user to customize their layouts much easier.
    
    Added config to ignore and replaced with example config (so people don't
    accidently commit it).

--- web/includes/generatetopplayers.php
+++ web/includes/generatetopplayers.php
@@ -1,24 +1,23 @@
 <?php
-$generated = "<table class=\"tg\" width=\"450px\">
+require 'includes/config.inc.php';
+
+$generated = "<table class=\"table table-striped\" width=\"450px\">
 	<tr>
-		<th class=\"tg-dilm\">#</th>
-		<th class=\"tg-dilm\" width=\"200\">Name</th>
-		<th class=\"tg-dilm\">Wins</th>
-		<th class=\"tg-dilm\">Losses</th>
-		<th class=\"tg-dilm\">W/L Ratio</th>
-		<th class=\"tg-dilm\">ELO Rank</th>
+		<th>#</th>
+		<th width=\"200\">Name</th>
+		<th>Wins</th>
+		<th>Losses</th>
+		<th>W/L Ratio</th>
+		<th>ELO Rank</th>
 	</tr>";
 
 $alt = 0;
 
-$server_limit = 'AND serverID=0';
-$server_id = '0';
-if (isset($_GET['serverid'])) {
-	$server_id = (int)$_GET['serverid'];
-	$server_limit = 'AND serverID='.(int)$_GET['serverid'];
-}
+$server_limit = '';
+if (isset($_GET['server_id']))
+	$server_limit = 'AND serverID='.(int)$_GET['server_id'];
 
-$run_query = "SELECT s1.*, (SELECT COUNT(*) FROM $mysql_table AS s2 WHERE s2.rating > s1.rating AND s2.wins+s2.losses > 200 $server_limit)+1 AS rank FROM $mysql_table AS s1 WHERE s1.wins+s1.losses > 200 $server_limit ORDER BY rating DESC LIMIT 0, 15";
+$run_query = "SELECT s1.*, (SELECT COUNT(*) FROM $mysql_table AS s2 WHERE s2.rating > s1.rating AND s2.wins+s2.losses > 100)+1 AS rank FROM $mysql_table AS s1 WHERE s1.wins+s1.losses > 100 $server_limit ORDER BY rating DESC LIMIT 0, 15";
 $query = mysqli_query($connect, $run_query);
 
 if (@$query){
@@ -39,38 +38,37 @@ if (@$query){
 		}
 
 		if ($alt == 0){
-			$generated .= "<tr><td class=\"tg-bsv2\">$rank</td>";
+			$generated .= "<tr><td>".$rank."</td>";
 
-			// NEED TO FIX THE SERVERID! Combine user stats.
 			if (isPlayerActive($lastTime) == true){
-				$generated .= "<td class=\"tg-bsv2\"><a href=\"index.php?id=".$accountID."&serverid=".$server_id."\">$name (Inactive)</a></td>";
+				$generated .= "<td><a href=\"index.php?id=".$accountID."\">$name (Inactive)</a></td>";
 			} else {
-				$generated .= "<td class=\"tg-bsv2\"><a href=\"index.php?id=".$accountID."&serverid=".$server_id."\">$name</a></td>";
+				$generated .= "<td><a href=\"index.php?id=".$accountID."\">$name</a></td>";
 			}
 
-			$generated .= "<td class=\"tg-bsv2\">$wins</td>
-							<td class=\"tg-bsv2\">$losses</td>
-							<td class=\"tg-bsv2\">$WL</td>
-							<td class=\"tg-bsv2\">$rating</td></tr>";
+			$generated .= "<td>$wins</td>
+							<td>$losses</td>
+							<td>$WL</td>
+							<td>$rating</td></tr>";
 			$alt = 1;
 		} else{
-			$generated .= "<tr><td class=\"tg-dilm\">$rank</td>";
+			$generated .= "<tr><td>$rank</td>";
 
 			if (isPlayerActive($lastTime) == true){
-				$generated .= "<td class=\"tg-dilm\"><a href=\"index.php?id=".$accountID."&serverid=".$server_id."\">$name (Inactive)</a></td>";
+				$generated .= "<td><a href=\"index.php?id=".$accountID."\">$name (Inactive)</a></td>";
 			} else{
-				$generated .= "<td class=\"tg-dilm\"><a href=\"index.php?id=".$accountID."&serverid=".$server_id."\">$name</a></td>";
+				$generated .= "<td><a href=\"index.php?id=".$accountID."\">$name</a></td>";
 			}
 
-			$generated .= "<td class=\"tg-dilm\">$wins</td>
-							<td class=\"tg-dilm\">$losses</td>
-							<td class=\"tg-dilm\">$WL</td>
-							<td class=\"tg-dilm\">$rating</td></tr>";
+			$generated .= "<td>$wins</td>
+							<td>$losses</td>
+							<td>$WL</td>
+							<td>$rating</td></tr>";
 			$alt = 0;
 		}
 		
 	}
-	$generated .= "</table><br>Ranked by ELO Rating.";
+	$generated .= "</table>";
 }
 echo $generated;
 ?>
--- web/search.php
+++ web/search.php
@@ -1,58 +1,120 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 
-<?php
-	require_once 'includes/config.inc.php';
-	echo "<title>$page_title</title>";
-	$time = microtime();
-	$time = explode(' ', $time);
-	$time = $time[1] + $time[0];
-	$start = $time;
-?>
 
-<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
-<link href="css/style.css" rel="stylesheet" type="text/css" media="all">
-</head>
-<body>
-<div class="container">
-	<div class="everything">
-		<div class="search">
-			<form action="search.php" method="GET">
-			<h6>Search player stats:</h6><input type="text" name="searchquery">
-			<input type="submit" value="Submit">
-			</form>
-		</div>
-
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<meta name="description" content="1v1 Arena Description"/>
+		<meta name="author" content="Your Name"/>
 		<?php
-		if (isset($_GET['searchquery']) && !empty($_GET['searchquery']) && strlen($_GET['searchquery'])>2){
-			$searchquery = htmlentities($_GET['searchquery']);
-			$run_query = "SELECT `accountID`, `serverID`, `name` FROM multi1v1_stats WHERE name LIKE '%".mysqli_real_escape_string($connect, $searchquery)."%'";
-			$query = mysqli_query($connect, $run_query);
-
-			echo "<h3>Searching for players with name \"$searchquery\".</h3>";
-			if (mysqli_num_rows($query) > 0){
-				echo "<div class=\"stats\">Players found:<br><ul>";
-				if ($query) {
-					while ($row = mysqli_fetch_assoc($query)){
-						$accountID = $row['accountID'];
-						$server_id = $row['serverID'];
-						$name = htmlentities($row['name']);
-                                                //FIX SERVER ID
-						echo "<h2><li><a href=\"index.php?id=$accountID&serverid=$server_id\">$name (Server $server_id)</a></li></h2>";
-					}
-				}
-				echo "</ul></div>";
-			} else {
-				die("No users found with name \"".$searchquery."\".");
-			}
-		} else {
-			die("<h3>Please enter a search term longer than 2 characters.</h3>");
-		}
+			require_once 'includes/config.inc.php';
+			require_once 'includes/utils.inc.php';
+			echo "<title>$page_title</title>";
+			$time = microtime();
+			$time = explode(' ', $time);
+			$time = $time[1] + $time[0];
+			$start = $time;
 		?>
 
-	</div>
-</div>
-</body>
+		<!-- Bootstrap core CSS -->
+		<link href="./css/bootstrap.css" rel="stylesheet"/>
+
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+		<link href="./css/ie10-viewport-bug-workaround.css" rel="stylesheet"/>
+
+		<!-- Custom styles for this template -->
+		<link href="./css/jumbotron-narrow.css" rel="stylesheet"/>
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
+	</head>
+
+	<body>
+			<div class="container">
+				<div class="header clearfix">
+					<nav>
+						<ul class="nav nav-pills pull-right">
+							<li role="presentation"><a href="index.php">Top Players</a></li>
+							<li role="presentation" class="active"><a href="search.php">Search</a></li>
+						</ul>
+					</nav>
+					<h3 class="text-muted">1v1 Arena</h3>
+				</div>
+
+				<div class="alert alert-info">
+					<h1>Hey!</h1>
+					<p><strong>You can customize this!</strong> To say whatever you'd like, or remove it entirely.</p>
+				</div>
+
+				<br/>
+
+				<div>
+					<div class="panel panel-primary">
+						<?php
+						if (isset($_GET['searchquery']) && !empty($_GET['searchquery']) && strlen($_GET['searchquery'])>2){
+
+							$searchquery = htmlentities($_GET['searchquery']);
+							$run_query = "SELECT `accountID`, `name` FROM multi1v1_stats WHERE name LIKE '%".mysqli_real_escape_string($connect, $searchquery)."%'";
+							$query = mysqli_query($connect, $run_query);
+							echo '<div class="panel-heading"><h3>Results for '.$searchquery.'</h3>';
+							include 'includes/search_header.html';
+							echo '</div><ul class="list-group">';
+
+							if (mysqli_num_rows($query) > 0){
+								if ($query) {
+									while ($row = mysqli_fetch_assoc($query)){
+										$accountID = $row['accountID'];
+										$name = htmlentities($row['name']);
+										echo '<li class="list-group-item"><a href="index.php?id='.$accountID.'">'.$name.'</a></li>';
+									}
+								}
+							} else {
+								echo '<li class="list-group-item list-group-item-danger"><strong>No users found with name "'.$searchquery.'".</strong></li>';
+							}
+
+							echo '</ul>';
+						} else {
+							echo '<div class="panel-heading"><h3>Search</h3>';
+							include 'includes/search_header.html';
+							echo '</div>';
+						}
+						?>
+					</div>
+				</div>
+			</div>
+
+			<footer class="footer">
+				<div class="container">
+					<center>
+						<?php
+						if (isset($_GET['id']) && !empty($_GET['id'])){
+							$time = microtime();
+							$time = explode(' ', $time);
+							$time = $time[1] + $time[0];
+							$finish = $time;
+							$total_time = round(($finish - $start), 4);
+							echo '<p>Page generated in '.$total_time.' seconds.</p>';
+						}
+						?>
+					</center>
+				</div>
+			</footer>
+
+		</div> <!-- /container -->
+
+
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+		<script src="./js/ie10-viewport-bug-workaround.js"></script>
+		<script src="./js/jquery-2.1.4.min.js"></script>
+		<script src="./js/bootstrap.min.js"></script>
+	</body>
+
 </html>
